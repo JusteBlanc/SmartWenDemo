287a2bb328d4775b1f7f2ca1b51e0a4c440a9667
JusteBlanc <loic.blanc@hotmail.fr>
2016-03-29 09:09:53 +0200

    initial commit

---
+++ script/script.js
@@ -0,0 +1,93 @@
+//Const
+var lineTreshold = 0.50;
+var zoom = 2.0;
+
+//Global
+var img, ctxInput, ctxGray, ctxLines;
+
+
+
+function detectLines(){
+    var imageData = ctxGray.getImageData(0, 0, img.width*zoom, img.height*zoom);
+    var pixels = imageData.data;  
+    var nbPixels = pixels.length/4;
+    
+    for (var y = 0; y < img.height*zoom;y++)
+    {
+        var isEmpty = true;
+        var percentFilled = 0;
+        for(var x = 0; x<img.width*zoom; x++)
+        {
+            if( pixels[x*4 + y*img.width*zoom *4] < 230)
+            {
+                percentFilled += 1/img.width*zoom;
+                 if(percentFilled > lineTreshold)
+                 {
+                     isEmpty = false;
+                 } 
+            }
+        }
+        //Coloration ligne vide
+        if(isEmpty)
+        {
+            for(var x = 0; x<img.width*zoom;x++)
+            {
+                pixels[x*4 + y*img.width*zoom*4] = 255; // r
+                pixels[x*4+ 1 + y*img.width*zoom*4] = 0; // v
+                pixels[x*4+ 2 + y*img.width*zoom*4] = 0; // b
+            }
+        }
+    }    
+    ctxLines.putImageData(imageData, 0, 0);
+}
+
+function loadInputImg(){
+    img = new Image();
+    img.src = 'image/imerir.bmp';
+    img.onload = function()
+    {
+        ctxInput.drawImage(img, 0, 0, img.width*zoom, img.height*zoom);
+        convertToGray();
+        detectLines();
+    }
+}
+
+function convertToGray()
+{
+    var imageData = ctxInput.getImageData(0, 0, img.width*zoom, img.height*zoom);
+    var pixels = imageData.data;  
+    var nbPixels = pixels.length/4;
+    
+    for (var i=0; i<nbPixels; i++) 
+    {
+        var average = 0.299* pixels[i*4] + 0.587* pixels[i*4+1] + 0.114*pixels[i*4+2];
+        //var average = (pixels[i*4] + pixels[i*4+1] + pixels[i*4+2]) /3 ;
+        
+        pixels[i*4] = average; // r
+        pixels[i*4+1] = average; // v
+        pixels[i*4+2] = average; // b
+        pixels[i*4+3] = 255; // alpha
+    }
+    
+    ctxGray.putImageData(imageData, 0, 0);
+}
+
+function ocradjs()
+{
+    var text = OCRAD(document.getElementById('cvs-input'));
+    console.log(text);
+}
+
+function initialize()
+{
+    ctxInput = document.getElementById('cvs-input').getContext('2d');
+    ctxGray = document.getElementById('cvs-gray').getContext('2d');
+    ctxLines = document.getElementById('cvs-lines').getContext('2d');
+    
+}
+
+$(document).ready(function(){
+    initialize();
+    loadInputImg();
+    ocradjs();
+});
